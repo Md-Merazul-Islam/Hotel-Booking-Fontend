dec3866af97339d6732599fd4889df474fdce577
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-06-22 09:46:13 +0600

    login,logout design done

--- js/main.js
+++ js/main.js
@@ -12,6 +12,44 @@
     spinner();
 
 
+    // if authenticated login, logout handler 
+    document.addEventListener('DOMContentLoaded',function(){
+        const authButtons = document.getElementById('auth-buttons');
+        const isAuthenticated = Boolean(localStorage.getItem('authToken'));
+        if (isAuthenticated){
+            authButtons.innerHTML=`
+                          
+                            <div class="d-flex justify-content-end align-items-center">
+                                <div class="balance pe-3">$0.00</div>
+                                <div class="btn-group dropstart pe-4">
+                                    <button type="button" class="btn btn-outline-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <img src="./img/user.png" class="r-img" alt="My profile">
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="balance.html">
+                                            <img src="./img/credit-cards.png" class="r-img" alt="Balance"> $0.00
+                                        </a></li>
+                                        <li><a class="dropdown-item" href="profile.html">
+                                            <img src="./img/user.png" class="r-img" alt="My profile"> Profile
+                                        </a></li>
+                                        <li><a class="dropdown-item" href="deposit.html">
+                                            <img src="./img/add-wallet.png" class="r-img" alt="Deposit"> Deposit
+                                        </a></li>
+                                        <li><a class="dropdown-item" href="#" onclick="logout()">
+                                            <img src="./img/logout.png" class="r-img" alt="Logout"> Logout
+                                        </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+            `;
+        }
+        function logout(){
+            localStorage.removeItem('authToken');
+            window.location.href ='index.html'
+        }
+    });
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -132,6 +170,7 @@
             }
         }
     });
+
 
 
 })(jQuery);
